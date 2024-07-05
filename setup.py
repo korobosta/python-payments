53315c9d10314b65d9c7ec7abdf3e4a5e8f1f9ef
Kevin Ongulu <kevinkorobosta@gmail.com>
2024-07-05 13:22:18 +0300

    read me file update

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
+# Read the contents of README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='koros_python_payments',
-    version='1.0.0',
+    name='koros-python-payments',
+    version='1.0.2',
     author='Kevin Ongulu',
     author_email='kevinongulu@gmail.com',
     description='A python package for connecting to payments platforms like MPESA,PayPal,RazorPay',
     #packages=find_packages(),
-    packages = ['mpesa'], 
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages = ['mpesa','paypal'], 
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
